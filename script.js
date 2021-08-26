0e208a801ce40f36e005dcf08c6d7edec7045aad
sechs <philippe.buteau@skema.edu>
2021-08-26 12:49:18 +0200

    exclusive prize section done

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const bg = document.querySelector(".welcome-background-one");
 const enterBtn = document.querySelector(".enter");
 const title = document.querySelector(".title");
+const boxUp = document.querySelector(".box-up");
+const box = document.querySelector(".box");
+const raffleTxt = document.querySelector(".raffle-txt");
 
 enterBtn.addEventListener("click", enterSite);
+boxUp.addEventListener("click", showSneaker);
 
+// Function for entering of the website
 function enterSite() {
   bg.classList.add("active");
   enterBtn.classList.add("active");
+  title.classList.add("active");
   setTimeout(() => {
-    title.classList.add("active");
-  }, 2000);
+    enterBtn.style.display = "none";
+  }, 400);
+}
+
+// Function for showing the sneaker prize
+function showSneaker() {
+  boxUp.classList.add("active");
+  box.classList.add("active");
+  raffleTxt.classList.toggle("active");
+  setTimeout(() => {
+    raffleTxt.classList.toggle("active");
+    raffleTxt.innerHTML = "A pair of Jordan 4 Fire Red, now that's a win !";
+  }, 400);
 }
